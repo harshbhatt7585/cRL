21bebd40ebf841354b32153569f3aba91b8290e6
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-07-12 21:54:33 +0530

    sample action and global iteration

--- env.c
+++ env.c
@@ -144,6 +144,20 @@ void take_action(SnakeENV* env, u32 action) {
     }
 }
 
+ACTION sample_action(matrix* probs) {
+    f32 r = prng_randf();
+    f32 cdf = 0.0f;
+
+    u32 size = probs->rows * probs->cols;
+    for(u32 i=0; i<size; i++) {
+        cdf += probs->data[i];
+        if(r <= cdf) {
+            return (ACTION)i;
+        }
+    }
+    return (ACTION)(size - 1);
+}
+
 
 void build_state_vector(
     matrix* in,
@@ -166,102 +180,111 @@ void train(
     SnakeENV* env,
     mem_arena* arena
 ) {
-    u32 EPOCHS = 10;
+    u32 EPOCHS = 512;
+    u32 microbatch = 64;
     u32 batch_size = 32;
     u32 rollout_size = 128;
     u32 episode_len = 100;
     ReplayBuffer buffer = {0};
 
-    for(u32 i = 0; i < rollout_size; i++) {
+    for (u32 epoch=0; epoch < EPOCHS; epoch++) {
 
-        reset_state(env);
-        // Rollout phase - collect experience from the model
-        for(u32 t_i = 0; t_i < episode_len; t_i++) {
-            // ACTION action = randn(4);
-            forward_pass(&model->forward_graph);
+        for(u32 i = 0; i < rollout_size; i++) {
 
-            matrix* probs = model->output->val;
-            f32 log_probs[probs->rows * probs->cols];
-            for(u32 i=0; i<probs->rows * probs->cols; i++) {
-                log_probs[i] = logf(probs->data[i]);
-            } 
-
-            ACTION action = argmax(model->output->val);
-            
-
-            // printf("Output: %u\n", action);
-            take_action(env, randn(4));
-            f32 reward = get_reward(env);
-            env->score += reward;
-            b32 is_game_over = game_over(env);
-
-            // printf("Env %d\n",env->x);
-            // printf("Env %d\n",env->y);
-            // printf("Env %llu\n",env->score);
-            // printf("GAME OVER: %u\n", is_game_over);  
-            
-            
-            if (is_game_over) {
-                reset_state(env);
-                break;
-            }
-
-            // Store the data in buffer
-            buffer.trajactories[i].states[t_i].x = env->x;
-            buffer.trajactories[i].states[t_i].y = env->y;
-            buffer.trajactories[i].food_states[t_i].x = env->food_pos_x;
-            buffer.trajactories[i].food_states[t_i].y = env->food_pos_y;
-            buffer.trajactories[i].actions[t_i] = action;
-            buffer.trajactories[i].rewards[t_i] = reward;
-            buffer.trajactories[i].dones[t_i] = is_game_over;
-            buffer.trajactories[i].len = t_i + 1;
-        }
-    }
-    // Training Phase
-    for(u32 epoch=0; epoch < EPOCHS; epoch++) {
-        
-        // Sample batch from buffer
-        u64 start_idx = randn( (u64)(BUFFER_SIZE - batch_size));
-        u64 end_idx = start_idx + batch_size;
-
-        for(u64 b=start_idx; b < end_idx; b++) {
-            Trajactory traj = buffer.trajactories[b];
-
-            matrix* policy_loss = create(arena, traj.len, 1); 
-
-
-            for (u32 t=0; t < traj.len; t++) {
-                State state_ = traj.states[t];
-                State food_state_ = traj.food_states[t];
-                ACTION action_ = traj.actions[t];
-                f32 reward_ = traj.rewards[t];
-                f32 return_ = 0;
-
-                for (u32 j=t; j <traj.len; j++) {
-                    return_ += traj.rewards[j];
-                }
-                
-                build_state_vector(
-                    model->input->val,
-                    state_,
-                    food_state_,
-                    env->cols,
-                    env->grid_size
-                );
-                
+            reset_state(env);
+            // Rollout phase - collect experience from the model
+            for(u32 t_i = 0; t_i < episode_len; t_i++) {
+                // ACTION action = randn(4);
                 forward_pass(&model->forward_graph);
-
-                f32 p = MAX(model->output->val->data[action_], 1e-8f);
-                policy_loss->data[t] = -logf(p) * return_;
-
+    
+                matrix* probs = model->output->val;
+                f32 log_probs[probs->rows * probs->cols];
+                for(u32 i=0; i<probs->rows * probs->cols; i++) {
+                    log_probs[i] = logf(probs->data[i]);
+                } 
+    
+                ACTION action = sample_action(probs);
                 
+    
+                // printf("Output: %u\n", action);
+                take_action(env, action);
+                f32 reward = get_reward(env);
+                env->score += reward;
+                b32 is_game_over = game_over(env);
+    
+                // printf("Env %d\n",env->x);
+                // printf("Env %d\n",env->y);
+                // printf("Env %llu\n",env->score);
+                // printf("GAME OVER: %u\n", is_game_over);  
+                
+                
+                if (is_game_over) {
+                    reset_state(env);
+                    break;
+                }
+    
+                // Store the data in buffer
+                buffer.trajactories[i].states[t_i].x = env->x;
+                buffer.trajactories[i].states[t_i].y = env->y;
+                buffer.trajactories[i].food_states[t_i].x = env->food_pos_x;
+                buffer.trajactories[i].food_states[t_i].y = env->food_pos_y;
+                buffer.trajactories[i].actions[t_i] = action;
+                buffer.trajactories[i].rewards[t_i] = reward;
+                buffer.trajactories[i].dones[t_i] = is_game_over;
+                buffer.trajactories[i].len = t_i + 1;
             }
-
-            model->cost->val = policy_loss;
-            printf("Loss: %f\n", sum(policy_loss));
-            backward_pass(&model->cost_graph);
         }
+        // Training Phase
+        for(u32 m=0; m < microbatch; m++) {
+            
+            // Sample batch from buffer
+            u64 start_idx = randn( (u64)(BUFFER_SIZE - batch_size));
+            u64 end_idx = start_idx + batch_size;
+    
+            for(u64 b=start_idx; b < end_idx; b++) {
+                Trajactory traj = buffer.trajactories[b];
+    
+                matrix* policy_loss = create(arena, traj.len, 1); 
+    
+    
+                for (u32 t=0; t < traj.len; t++) {
+                    State state_ = traj.states[t];
+                    State food_state_ = traj.food_states[t];
+                    ACTION action_ = traj.actions[t];
+                    f32 reward_ = traj.rewards[t];
+                    f32 return_ = 0;
+    
+                    for (u32 j=t; j <traj.len; j++) {
+                        return_ += traj.rewards[j];
+                    }
+                    if(t==(traj.len-1)) {
+                        printf("Return: %f\n", return_);
+                    }
+                    
+                    build_state_vector(
+                        model->input->val,
+                        state_,
+                        food_state_,
+                        env->cols,
+                        env->grid_size
+                    );
+                    
+                    forward_pass(&model->forward_graph);
+    
+                    f32 p = MAX(model->output->val->data[action_], 1e-8f);
+                    policy_loss->data[t] = -logf(p) * return_;
+
+                }
+                model->cost->val = policy_loss;
+                // printf("Loss: %f\n", sum(policy_loss) / batch_size);
+
+                backward_pass(&model->cost_graph);
+            }
+        }
+
     }
+
+    
 
 }
 
