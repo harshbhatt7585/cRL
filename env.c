f8fac26fcdf6af47e0380914edbcede966746fca
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-07-17 23:29:10 +0530

    redundant

--- env.c
+++ env.c
@@ -368,8 +368,8 @@ int main(void) {
     model_state* model = PUSH_STRUCT(arena, model_state);
 
     create_actor_model(arena, model);
-    model->forward_graph = build_graph3(arena, model, model->output);
-    model->cost_graph = build_graph3(arena, model, model->cost);
+    model->forward_graph = build_graph(arena, model, model->output);
+    model->cost_graph = build_graph(arena, model, model->cost);
 
 
     SnakeENV* env = create_env(36);
